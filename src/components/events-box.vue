c46ff07a15e2845302147f57457b0bc2a8cc1abb
bazelski <bazelski@gmail.com>
2019-10-18 20:28:28 +0200

    IconPos  commit

--- src/components/events-box.vue
+++ src/components/events-box.vue
@@ -7,8 +7,8 @@
       @click.stop="eventClicked(event)"
       v-for="(event, index) in eventList"
     >
-      <div class="event-title">
-        <fa class="event-icon mr-1" :class="event.iconColor" :icon="event.icon" /> {{ event.title }}
+      <div class="event-title flex">
+        <div><fa class="event-icon mr-1" :class="event.iconColor" :icon="event.icon" ></fa><div> {{ event.title }}</div>
       </div>
 
     </div>
